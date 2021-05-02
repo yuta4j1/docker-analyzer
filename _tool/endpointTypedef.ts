f20516a73c9dd71b32de77ed08ede078a1f0b8a1
yuta4j1 <kascado.ys10@gmail.com>
2021-05-02 15:43:35 +0900

    type-def

---
+++ _tool/endpointTypedef.ts
@@ -0,0 +1,45 @@
+import http from 'http'
+import { convertToCamelcase } from '../src/helper/json'
+import { typedefGen } from 'typedef-gen'
+
+// Docker APIへのリクエスト
+const getRequest = async <T>(url: string): Promise<T> => {
+  return new Promise<T>((resolve, reject) => {
+    const clientRequest = http.request(
+      {
+        socketPath: '/var/run/docker.sock',
+        path: url,
+      },
+      (res) => {
+        res.setEncoding('utf8')
+        let chunk = ''
+        res.on('data', (data) => (chunk += data))
+
+        res.on('end', () => {
+          try {
+            const parsed = JSON.parse(chunk)
+            resolve(convertToCamelcase(parsed))
+          } catch (err) {
+            console.error(err)
+          }
+        })
+        res.on('error', (err) => {
+          console.error(err)
+          reject(err)
+        })
+      }
+    )
+    clientRequest.end()
+  })
+}
+
+const endpointTypedefGen = () => {
+  getRequest('/v1.40/containers/json?all=1')
+    .then((res) => {
+      console.log("return res", res)
+      typedefGen(res)
+    })
+    .catch((err) => console.error(err))
+}
+
+endpointTypedefGen()
